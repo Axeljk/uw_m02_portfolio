23d93ac81ba2553977103060ce267c490901dbc9
axeljk <axeljkern@yahoo.com>
2022-06-22 15:35:05 -0400

    Adds dark mode option.

---
+++ assets/src/script.js
@@ -0,0 +1,23 @@
+console.log("hmm");
+
+function myFunction() {
+	const root = document.documentElement;
+	let cBase = getComputedStyle(root).getPropertyValue("--base");
+	let cDBase = getComputedStyle(root).getPropertyValue("--dBase");
+	let cBaseTmp = cBase;
+
+	let cPrimary = getComputedStyle(root).getPropertyValue("--primary");
+	let cDPrimary = getComputedStyle(root).getPropertyValue("--dPrimary");
+	let cPrimaryTmp = cPrimary;
+
+	let cSecondary = getComputedStyle(root).getPropertyValue("--secondary");
+	let cDSecondary = getComputedStyle(root).getPropertyValue("--dSecondary");
+	let cSecondaryTmp = cSecondary;
+
+	root.style.setProperty("--base", cDBase);
+	root.style.setProperty("--dBase", cBaseTmp);
+	root.style.setProperty("--primary", cDPrimary);
+	root.style.setProperty("--dPrimary", cPrimaryTmp);
+	root.style.setProperty("--secondary", cDSecondary);
+	root.style.setProperty("--dSecondary", cSecondaryTmp);
+ }
